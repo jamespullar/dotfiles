e911b005dc128979158621c2b9e7eff696faea4d
James Pullar <james@pullar.me>
2025-05-15 12:16:08 -0700

    Update .config/qutebrowser/config.py

--- private_dot_config/private_qutebrowser/config.py
+++ private_dot_config/private_qutebrowser/config.py
@@ -15,7 +15,7 @@ c.auto_save.session = True
 # 
 c.url.start_pages = ["qute://bookmarks/"]
 
-c.colors.webpage.darkmode.policy.images = "smart"
+c.colors.webpage.darkmode.policy.images = "never"
 
 # }}}
 
