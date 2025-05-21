fd9c1b6f68d6a7fabd0f8edf6890e60eba2863fc
James Pullar <james@pullar.me>
2025-05-21 09:15:31 -0700

    Update .config/nvim/lua/config/bindings.lua

--- private_dot_config/nvim/lua/config/bindings.lua
+++ private_dot_config/nvim/lua/config/bindings.lua
@@ -8,4 +8,10 @@ local k = vim.keymap
 
 g.mapleader = " " -- Set vim leader to <space>
 
-k.set('n', '<Leader>', 'za')
+k.set('n', '<Leader>', 'za') -- Toggle folds with leader key
+
+-- Split navigation using Ctrl+hjkl
+k.set('n', '<C-h>', '<C-w>h')
+k.set('n', '<C-j>', '<C-w>j')
+k.set('n', '<C-k>', '<C-w>k')
+k.set('n', '<C-l>', '<C-w>l')
