2b90c800ce09c51f82774e7925774c85f2e7c373
James Pullar <james@pullar.me>
2025-05-25 14:00:59 -0700

    Update .config/nvim/lua/config/plugins/lspconfig.lua

--- private_dot_config/nvim/lua/config/plugins/lspconfig.lua
+++ private_dot_config/nvim/lua/config/plugins/lspconfig.lua
@@ -24,10 +24,10 @@ return {
       vim.diagnostic.config({
           signs = {
               text = {
-                  [vim.diagnostic.severity.ERROR] = " "
-                  [vim.diagnostic.severity.WARN] = " "
-                  [vim.diagnostic.severity.INFO] = " "
-                  [vim.diagnostic.severity.HINT] = "󰠠 "
+                  [vim.diagnostic.severity.ERROR] = " ",
+                  [vim.diagnostic.severity.WARN] = " ",
+                  [vim.diagnostic.severity.INFO] = " ",
+                  [vim.diagnostic.severity.HINT] = "󰠠 ",
               }
           }
       })
