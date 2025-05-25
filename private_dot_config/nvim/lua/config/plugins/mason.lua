e965fce01dd79f508c4d947363f12432deca0895
James Pullar <james@pullar.me>
2025-05-25 14:08:05 -0700

    Update .config/nvim/lua/config/plugins/lspconfig.lua Add .config/nvim/lua/config/plugins/mason.lua

---
+++ private_dot_config/nvim/lua/config/plugins/mason.lua
@@ -0,0 +1,58 @@
+return {
+  -- Mason for managing LSP servers
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    build = ":MasonUpdate",
+    opts = {
+      ensure_installed = {
+        "gopls",           -- Go language server
+        "gofumpt",         -- Go formatter
+        "golangci-lint",   -- Go linter
+        "delve",           -- Go debugger
+      },
+    },
+    config = function(_, opts)
+      require("mason").setup(opts)
+      local mr = require("mason-registry")
+      mr:on("package:install:success", function()
+        vim.defer_fn(function()
+          require("lazy.core.handler.event").trigger({
+            event = "FileType",
+            buf = vim.api.nvim_get_current_buf(),
+          })
+        end, 100)
+      end)
+      local function ensure_installed()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
+        end
+      end
+      if mr.refresh then
+        mr.refresh(ensure_installed)
+      else
+        ensure_installed()
+      end
+    end,
+  },
+  
+  -- Bridge between Mason and lspconfig
+  {
+    "williamboman/mason-lspconfig.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {
+      ensure_installed = {
+        "gopls", -- Ensure gopls is installed
+      },
+      automatic_installation = true,
+    },
+  },
+}
