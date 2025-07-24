94684863e38c0b4a9f74fea2c2f427b7dd5c4f7d
James Pullar <james@pullar.me>
2025-07-24 11:35:56 -0700

    Updating configs

--- private_dot_config/private_qutebrowser/config.py
+++ private_dot_config/private_qutebrowser/config.py
@@ -28,9 +28,22 @@ c.qt.force_software_rendering = 'none'
 c.content.webgl = False
 c.content.autoplay = False
 
+c.editor.command = [ 'foot', '--title="qutepicker"', 'vim', '{file}',  '+normal {line}G{column0}l' ]
+c.fileselect.handler = 'external'
+c.fileselect.single_file.command = [ 'foot','--title="qutepicker"', 'yazi', '--chooser-file={}' ]
+c.fileselect.multiple_files.command = [ 'foot', '--title="qutepicker"', 'yazi', '--chooser-file={}' ]
+
 # Enable WebGL only for specific trusted sites
 config.set('content.webgl', True, '*://underscorecraft.com:8123/*')
 config.set('content.webgl', True, '*://alltrails.com/*')
+
+# }}}
+
+
+# key bindings {{{
+
+config.bind("<Ctrl-d>", "cmd-run-with-count 20 scroll down")
+config.bind("<Ctrl-u>", "cmd-run-with-count 20 scroll up")
 
 # }}}
 
